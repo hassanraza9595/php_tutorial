a4ffb9189f358dff0ad90fa352746ba68f4db02f
Hassan Raza <56672361+hassanraza9595@users.noreply.github.com>
2020-08-06 11:09:34 +0500

    php basic added from variable to looping

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+     
+    <?php
+
+    // print statement
+    // echo "Hello World\n";
+    // variable defining
+    // $fname = "Hassan Raza";
+    // $lname = "Gadhi";
+
+    //  $number = 300;
+    // echo $number;
+
+    // concatenate operator .
+    // echo $fname. " ".$lname;
+
+    // Using the strlen() function
+
+    // echo strlen($fname);
+
+    // Using the strpos() function
+
+    // echo strpos($fname,'n');
+
+    // Operators
+
+    // echo 4+6;
+    // echo 6-4;
+    // echo 3*3;
+    // echo 12/3;
+    // echo 12%5;
+    // echo ++$number;
+    // echo --$number;
+
+   // Comparsion Operator
+
+//    echo $number == 300;
+//    echo $number > 300;
+//    echo $number >= 300;
+//    echo $number <=300;
+//    echo $number != 300;
+
+   // Assignment Operator
+
+//    echo $number += $number;
+//    echo $number -= 300;
+//    echo $number *= $number;
+//    echo $number /= $number;
+//    echo $number %= $number;
+  
+   // logical Operator
+
+
+//    echo ( 300 > 200 && 300 < 400);
+//    echo ( 300 > 300 || 300 || 400);
+//    echo !( 300 > 200 && 300 < 400);
+
+   // Conditions Statements
+
+//    $test = 3;
+
+//    if($test == 4){
+//        echo "True";
+//    }
+
+//    elseif ($test == 3)
+//    {
+//        echo "Else if win";
+//    }
+
+//    else {
+//        echo "false";
+//    }
+
+//    $d = date("D");
+//    if($d == "Thu")
+//         echo "Today is a thursday";
+//    else 
+//         echo "Not a day";     
+
+// Switch Statement
+
+//    $number = 0;
+
+//    switch($number){
+//        case 1:
+//         echo "Case 1";
+//        break;
+
+//        case 2:
+//         echo "Case 2";
+//        break;
+
+//        case 3:
+//         echo "Case 3";
+//        break;
+
+//        default:
+//           echo "Default case";
+//    }
+
+      // Arrays
+
+      // Numeric Array
+
+    //   $names  = array("Hassan", "Raza", "Gadhi");
+ 
+    //   $names[0] = "Ali";
+
+    //   echo $names[0];
+
+      // Associate Array with key and value
+
+      $ages = array("Hassan"=>21, "Junaid" => 22, "Asif"=>26);
+
+     // echo $ages["Junaid"];
+     
+
+    //  for( $var = 0; $var<3; $var++){
+    //      echo $names[$var];
+    //  }
+
+    // Multidimensional Array
+
+    // $students = array(
+    //     "Student"=> array(
+    //         "Name"=> "Hassan",
+    //         "RollNo"=> 46,
+    //         "Batch"=> 2018
+    //     ),
+
+    //     "Student2"=> array(
+    //         "Name"=> "Junaid",
+    //         "RollNo"=> 50,
+    //         "Batch"=> 2018
+    //     )
+    //     );
+
+    //  echo $students['Student2']['Batch'];
+
+    // Looping 
+
+
+    // for loop
+
+    // for($i = 0; $i<10; $i++){
+     
+    //     echo $i;
+    // }
+
+    // while loop
+
+    // $num = 1;
+
+    // while($num<10){
+    //     echo $num;
+    //     $num++; 
+    // }
+
+    // do while loop
+
+    // $num = 1;
+
+    // do{
+    //     echo $num;
+    //     $num++;
+    // }
+    // while($num<10);
+
+    // Foreach
+
+    // $num = array(1,2,3,4,5);
+
+    // foreach($num as $number){
+    //     echo $number. " <br />";
+    // }
+
+
+    
+
+
+    
+    
+
+    ?>
+
+
+
+   
+</body>
+</html>
